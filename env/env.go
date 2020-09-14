7fc4e207303c15b018b7a4321c9f0d3c9bbddb95
zainul <zainulmasadi90@gmail.com>
2020-09-14 11:14:38 +0700

    add envi get mode and get path

--- env/env.go
+++ env/env.go
@@ -6,3 +6,13 @@ const (
 	Staging     = "staging"
 	Production  = "production"
 )
+
+//GetEnvMode is represent of environment mode currently applied
+func GetEnvMode() string {
+	return os.Getenv("ENV_MODE")
+}
+
+//GetAppPath is represent of get the path of application service
+func GetAppPath() string {
+	return os.Getenv("APP_PATH")
+}
