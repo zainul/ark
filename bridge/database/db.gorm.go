2c19a40d4464fe6b48ec3e624e77b5f6182f6be7
zainul <zainulmasadi90@gmail.com>
2020-11-25 09:07:28 +0700

    separate master and follower

--- bridge/database/db.gorm.go
+++ bridge/database/db.gorm.go
@@ -12,30 +12,34 @@ import (
 )
 
 type jinzhuGorm struct {
-	dbs map[DBType]*gorm.DB
-	mut sync.RWMutex
+	dbMaster   *gorm.DB
+	dbFollower *gorm.DB
+	mut        sync.RWMutex
 }
 
 func newGorm(ctx context.Context, dialect string, cfg Config) DB {
 	var err error
-	dbs := make(map[DBType]*gorm.DB)
-	dbs[master], err = apmgorm.Open(dialect, cfg.MasterConn)
+	var dbM, dbS *gorm.DB
+
+	dbM, err = apmgorm.Open(dialect, cfg.MasterConn)
 
 	if err != nil {
 		log.Fatal("Failed to init db", err)
 	}
 
-	dbs[slave], err = apmgorm.Open(dialect, cfg.SlaveConn)
+	dbS, err = apmgorm.Open(dialect, cfg.SlaveConn)
 
 	if err != nil {
 		log.Fatal("Failed to init db", err)
 	}
 
-	dbs[master].LogMode(cfg.LogMode)
-	dbs[slave].LogMode(cfg.LogMode)
+	dbM.LogMode(cfg.LogMode)
+	dbS.LogMode(cfg.LogMode)
 
 	return &jinzhuGorm{
-		dbs: dbs,
+		dbMaster:   dbM,
+		dbFollower: dbS,
+		mut:        sync.RWMutex{},
 	}
 
 }
@@ -44,7 +48,7 @@ func (g *jinzhuGorm) RunAsUnit(action func(tx interface{}) error) error {
 	g.mut.Lock()
 	defer g.mut.Unlock()
 	var errAction error
-	g.dbs[master].Transaction(func(tx *gorm.DB) error {
+	errAction = g.dbMaster.Transaction(func(tx *gorm.DB) error {
 		if errAction = action(tx); errAction != nil {
 			return errAction
 		}
@@ -56,26 +60,26 @@ func (g *jinzhuGorm) RunAsUnit(action func(tx interface{}) error) error {
 func (g *jinzhuGorm) Create(ctx context.Context, data interface{}, txs ...interface{}) error {
 	g.mut.Lock()
 	defer g.mut.Unlock()
-	g.dbs[master] = apmgorm.WithContext(ctx, g.dbs[master])
+	g.dbMaster = apmgorm.WithContext(ctx, g.dbMaster)
 
 	if len(txs) > 0 && txs[0] != nil {
 		tx, _ := txs[0].(*gorm.DB)
 		return tx.Create(data).Error
 	}
-	return g.dbs[master].Create(data).Error
+	return g.dbMaster.Create(data).Error
 }
 
 func (g *jinzhuGorm) EntityBy(ctx context.Context, field string, value interface{}, target interface{}, txs ...interface{}) error {
 	g.mut.Lock()
 	defer g.mut.Unlock()
-	g.dbs[slave] = apmgorm.WithContext(ctx, g.dbs[slave])
+	g.dbFollower = apmgorm.WithContext(ctx, g.dbFollower)
 
 	if len(txs) > 0 && txs[0] != nil {
 		tx, _ := txs[0].(*gorm.DB)
 		return tx.Where(map[string]interface{}{field: value}).Find(target).Error
 	}
 
-	return g.dbs[slave].Where(map[string]interface{}{field: value}).Find(target).Error
+	return g.dbFollower.Where(map[string]interface{}{field: value}).Find(target).Error
 }
 
 func (g *jinzhuGorm) Update(ctx context.Context, table string, data map[string]interface{}, whereCondition map[string]interface{}, txs ...interface{}) error {
@@ -104,13 +108,13 @@ func (g *jinzhuGorm) Update(ctx context.Context, table string, data map[string]i
 
 	vals = append(vals, whereVals...)
 
-	g.dbs[master] = apmgorm.WithContext(ctx, g.dbs[master])
+	g.dbMaster = apmgorm.WithContext(ctx, g.dbMaster)
 
 	if len(txs) > 0 && txs[0] != nil {
 		tx, _ := txs[0].(*gorm.DB)
 		return tx.Exec(query, vals...).Error
 	}
-	return g.dbs[master].Exec(query, vals...).Error
+	return g.dbMaster.Exec(query, vals...).Error
 }
 
 func (g *jinzhuGorm) Delete(ctx context.Context, table string, whereCondition map[string]interface{}, txs ...interface{}) error {
@@ -126,35 +130,35 @@ func (g *jinzhuGorm) Delete(ctx context.Context, table string, whereCondition ma
 	}
 
 	query = query + strings.Join(whereField, " AND ")
-	g.dbs[master] = apmgorm.WithContext(ctx, g.dbs[master])
+	g.dbMaster = apmgorm.WithContext(ctx, g.dbMaster)
 
 	if len(txs) > 0 && txs[0] != nil {
 		tx, _ := txs[0].(*gorm.DB)
 		return tx.Exec(query, whereVals...).Error
 	}
-	return g.dbs[master].Exec(query, whereVals...).Error
+	return g.dbMaster.Exec(query, whereVals...).Error
 }
 
 func (g *jinzhuGorm) QueryExec(ctx context.Context, txp interface{}, query string, args ...interface{}) error {
 	g.mut.Lock()
 	defer g.mut.Unlock()
-	g.dbs[master] = apmgorm.WithContext(ctx, g.dbs[master])
+	g.dbMaster = apmgorm.WithContext(ctx, g.dbMaster)
 
 	if txp != nil {
 		tx, _ := txp.(*gorm.DB)
 		return tx.Exec(query, args...).Error
 	}
-	return g.dbs[master].Exec(query, args...).Error
+	return g.dbMaster.Exec(query, args...).Error
 }
 
 func (g *jinzhuGorm) QueryRaw(ctx context.Context, txp interface{}, target interface{}, sql string, values ...interface{}) error {
 	g.mut.Lock()
 	defer g.mut.Unlock()
-	g.dbs[master] = apmgorm.WithContext(ctx, g.dbs[master])
+	g.dbMaster = apmgorm.WithContext(ctx, g.dbMaster)
 
 	if txp != nil {
 		tx, _ := txp.(*gorm.DB)
 		return tx.Raw(sql, values).Find(target).Error
 	}
-	return g.dbs[slave].Raw(sql, values).Find(target).Error
+	return g.dbMaster.Raw(sql, values).Find(target).Error
 }
