0be02459715a6fbb791b56bc9e9363bc3c42d9b6
zainul <zainulmasadi90@gmail.com>
2020-12-01 14:43:40 +0700

    update library

--- bridge/database/db.gorm.go
+++ bridge/database/db.gorm.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"strings"
-	"sync"
 
 	"github.com/jinzhu/gorm"
 	"go.elastic.co/apm/module/apmgorm"
@@ -14,7 +13,6 @@ import (
 type jinzhuGorm struct {
 	dbMaster   *gorm.DB
 	dbFollower *gorm.DB
-	mut        sync.RWMutex
 }
 
 func newGorm(ctx context.Context, dialect string, cfg Config) DB {
@@ -39,14 +37,11 @@ func newGorm(ctx context.Context, dialect string, cfg Config) DB {
 	return &jinzhuGorm{
 		dbMaster:   dbM,
 		dbFollower: dbS,
-		mut:        sync.RWMutex{},
 	}
 
 }
 
 func (g *jinzhuGorm) RunAsUnit(action func(tx interface{}) error) error {
-	g.mut.Lock()
-	defer g.mut.Unlock()
 	var errAction error
 	errAction = g.dbMaster.Transaction(func(tx *gorm.DB) error {
 		if errAction = action(tx); errAction != nil {
@@ -58,8 +53,7 @@ func (g *jinzhuGorm) RunAsUnit(action func(tx interface{}) error) error {
 }
 
 func (g *jinzhuGorm) Create(ctx context.Context, data interface{}, txs ...interface{}) error {
-	g.mut.Lock()
-	defer g.mut.Unlock()
+
 	g.dbMaster = apmgorm.WithContext(ctx, g.dbMaster)
 
 	if len(txs) > 0 && txs[0] != nil {
@@ -70,8 +64,7 @@ func (g *jinzhuGorm) Create(ctx context.Context, data interface{}, txs ...interf
 }
 
 func (g *jinzhuGorm) EntityBy(ctx context.Context, field string, value interface{}, target interface{}, txs ...interface{}) error {
-	g.mut.Lock()
-	defer g.mut.Unlock()
+
 	g.dbFollower = apmgorm.WithContext(ctx, g.dbFollower)
 
 	if len(txs) > 0 && txs[0] != nil {
@@ -83,8 +76,7 @@ func (g *jinzhuGorm) EntityBy(ctx context.Context, field string, value interface
 }
 
 func (g *jinzhuGorm) Update(ctx context.Context, table string, data map[string]interface{}, whereCondition map[string]interface{}, txs ...interface{}) error {
-	g.mut.Lock()
-	defer g.mut.Unlock()
+
 	query := fmt.Sprintf("UPDATE %s SET ", table)
 
 	vals := make([]interface{}, 0)
@@ -118,8 +110,7 @@ func (g *jinzhuGorm) Update(ctx context.Context, table string, data map[string]i
 }
 
 func (g *jinzhuGorm) Delete(ctx context.Context, table string, whereCondition map[string]interface{}, txs ...interface{}) error {
-	g.mut.Lock()
-	defer g.mut.Unlock()
+
 	query := fmt.Sprintf("DELETE %s WHERE", table)
 	whereVals := make([]interface{}, 0)
 	whereField := make([]string, 0)
@@ -140,8 +131,7 @@ func (g *jinzhuGorm) Delete(ctx context.Context, table string, whereCondition ma
 }
 
 func (g *jinzhuGorm) QueryExec(ctx context.Context, txp interface{}, query string, args ...interface{}) error {
-	g.mut.Lock()
-	defer g.mut.Unlock()
+
 	g.dbMaster = apmgorm.WithContext(ctx, g.dbMaster)
 
 	if txp != nil {
@@ -152,8 +142,7 @@ func (g *jinzhuGorm) QueryExec(ctx context.Context, txp interface{}, query strin
 }
 
 func (g *jinzhuGorm) QueryRaw(ctx context.Context, txp interface{}, target interface{}, sql string, values ...interface{}) error {
-	g.mut.Lock()
-	defer g.mut.Unlock()
+
 	g.dbMaster = apmgorm.WithContext(ctx, g.dbMaster)
 
 	if txp != nil {
